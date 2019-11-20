3c4c9623556987754739d3bbb0711423da682b6a
henrikvalle <48520733+henrikvalle@users.noreply.github.com>
2019-11-20 18:07:22 -0300

    adicionando htmls

---
+++ site/externo.js
@@ -0,0 +1,15 @@
+ 
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+ var instance = M.Carousel.init({
+    fullWidth: true
+  });
+
+  //  document.addEventListener('DOMContentLoaded', function() {
+  //   var elems = document.querySelectorAll('.carousel');
+  //   var instances = M.Carousel.init(elems);
+  // });
